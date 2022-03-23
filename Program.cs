c9d05ba301d8954cda4a2556db545499c2beb43d
furaihan <28698437+furaihan@users.noreply.github.com>
2022-03-23 13:47:41 +0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Taxi
+{
+    static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Taxi taxi = new();
+            taxi.DriverName = "Budi";
+            taxi.OnDuty = true;
+            taxi.NumPassengger = 3;
+            taxi.TaxiInfo();
+            Console.WriteLine();
+            taxi.PickupPassenger();
+            taxi.DropOffPassenger();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Taxi.cs
@@ -0,0 +1,23 @@
+﻿namespace Taxi
+{
+    public class Taxi
+    {
+        public string? DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassengger { get; set; }
+        public void TaxiInfo()
+        {
+            Console.WriteLine($"Driver Name: {DriverName}");
+            Console.WriteLine($"On Duty: {(OnDuty ? "Yes" : "No")}");
+            Console.WriteLine($"Number of Passenger: {NumPassengger}");
+        }
+        public void PickupPassenger()
+        {
+            Console.WriteLine($"{DriverName} sedang menjemput penumpang");
+        }
+        public void DropOffPassenger()
+        {
+            Console.WriteLine($"{DriverName} selesai mengantar penumpang");
+        }
+    }
+}
